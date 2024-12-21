38d0117aa4ca3dace0f8af293944ec401d2f2c6b
Jeesu-Jung <jisu.jung5@gmail.com>
2024-12-21 18:03:50 +0900

    feat: pinecone, langchain, upstage for rag system
    upstage embedding and generation.

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
+import os
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-from openai import AsyncOpenAI
-import os
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List, Optional
+from langchain_upstage import ChatUpstage
+from langchain_upstage import UpstageEmbeddings
+from langchain_upstage import UpstageDocumentParseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
+from langchain.chains import RetrievalQA
+from fastapi import BackgroundTasks
+
+# upstage models
+chat_upstage = ChatUpstage()
+embedding_upstage = UpstageEmbeddings(model="embedding-query")
+
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+pc = Pinecone(api_key=pinecone_api_key)
+index_name = "galaxy-a35"
+pdf_path = "Galaxy_A_35.pdf"
 
 load_dotenv()
-openai = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# create new index
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension=4096,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+
+pinecone_vectorstore = PineconeVectorStore(index=pc.Index(index_name), embedding=embedding_upstage)
+
+pinecone_retriever = pinecone_vectorstore.as_retriever(
+    search_type='mmr',  # default : similarity(유사도) / mmr 알고리즘
+    search_kwargs={"k": 3}  # 쿼리와 관련된 chunk를 3개 검색하기 (default : 4)
+)
 
 app = FastAPI()
 
@@ -34,52 +66,91 @@ class ChatRequest(BaseModel):
     messages: List[ChatMessage]  # Entire conversation for naive mode
 
 
+class MessageRequest(BaseModel):
+    message: str
+
+
 @app.post("/chat")
-async def chat_endpoint(req: ChatRequest):
-    # Assume the entire conversation (including a system message) is sent by the client.
-    # Example: messages might look like:
-    # [{"role":"system","content":"You are a helpful assistant."}, {"role":"user","content":"Hello"}]
+async def chat_endpoint(req: MessageRequest):
+    qa = RetrievalQA.from_chain_type(llm=chat_upstage,
+                                     chain_type="stuff",
+                                     retriever=pinecone_retriever,
+                                     return_source_documents=True)
 
-    response = await openai.chat.completions.create(
-        model="gpt-4o-mini", messages=req.messages
+    result = qa(req.message)
+    return {"reply": result['result']}
+
+
+# @app.post("/assistant")
+# async def assistant_endpoint(req: AssistantRequest):
+#     assistant = await openai.beta.assistants.retrieve("asst_tc4AhtsAjNJnRtpJmy1gjJOE")
+#
+#     if req.thread_id:
+#         # We have an existing thread, append user message
+#         await openai.beta.threads.messages.create(
+#             thread_id=req.thread_id, role="user", content=req.message
+#         )
+#         thread_id = req.thread_id
+#     else:
+#         # Create a new thread with user message
+#         thread = await openai.beta.threads.create(
+#             messages=[{"role": "user", "content": req.message}]
+#         )
+#         thread_id = thread.id
+#
+#     # Run and wait until complete
+#     await openai.beta.threads.runs.create_and_poll(
+#         thread_id=thread_id, assistant_id=assistant.id
+#     )
+#
+#     # Now retrieve messages for this thread
+#     # messages.list returns an async iterator, so let's gather them into a list
+#     all_messages = [
+#         m async for m in openai.beta.threads.messages.list(thread_id=thread_id)
+#     ]
+#     print(all_messages)
+#
+#     # The assistant's reply should be the last message with role=assistant
+#     assistant_reply = all_messages[0].content[0].text.value
+#
+#     return {"reply": assistant_reply, "thread_id": thread_id}
+
+
+@app.get("/health")
+@app.get("/")
+async def health_check():
+    return {"status": "ok"}
+
+
+def embed():
+    print("start")
+    document_parse_loader = UpstageDocumentParseLoader(
+        pdf_path,
+        output_format='html',  # 결과물 형태 : HTML
+        coordinates=False)  # 이미지 OCR 좌표계 가지고 오지 않기
+
+    docs = document_parse_loader.load()
+
+    # Split the document into chunks
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100)
+
+    # Embed the splits
+
+    splits = text_splitter.split_documents(docs)
+
+    PineconeVectorStore.from_documents(
+        splits, embedding_upstage, index_name=index_name
     )
-    assistant_reply = response.choices[0].message.content
-    return {"reply": assistant_reply}
+    print("end")
 
 
-@app.post("/assistant")
-async def assistant_endpoint(req: AssistantRequest):
-    assistant = await openai.beta.assistants.retrieve("asst_tc4AhtsAjNJnRtpJmy1gjJOE")
-
-    if req.thread_id:
-        # We have an existing thread, append user message
-        await openai.beta.threads.messages.create(
-            thread_id=req.thread_id, role="user", content=req.message
-        )
-        thread_id = req.thread_id
-    else:
-        # Create a new thread with user message
-        thread = await openai.beta.threads.create(
-            messages=[{"role": "user", "content": req.message}]
-        )
-        thread_id = thread.id
-
-    # Run and wait until complete
-    await openai.beta.threads.runs.create_and_poll(
-        thread_id=thread_id, assistant_id=assistant.id
-    )
-
-    # Now retrieve messages for this thread
-    # messages.list returns an async iterator, so let's gather them into a list
-    all_messages = [
-        m async for m in openai.beta.threads.messages.list(thread_id=thread_id)
-    ]
-    print(all_messages)
-
-    # The assistant's reply should be the last message with role=assistant
-    assistant_reply = all_messages[0].content[0].text.value
-
-    return {"reply": assistant_reply, "thread_id": thread_id}
+@app.post("/embed")
+async def load_pdf():
+    embed()
+    return {"status": "ok"}
 
 
 if __name__ == "__main__":
